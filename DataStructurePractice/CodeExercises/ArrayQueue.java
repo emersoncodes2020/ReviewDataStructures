f33d86b04625a19a9ff27010acc3aaecd12022dd
emersoncodes2020 <70417185+emersoncodes2020@users.noreply.github.com>
2021-02-21 07:44:18 -0500

    Queues
    Added notes and exercise for queues. a first in first out data structure

---
+++ DataStructurePractice/CodeExercises/ArrayQueue.java
@@ -0,0 +1,47 @@
+package CodeExercises;
+
+import java.util.Arrays;
+
+public class ArrayQueue {
+    private int[] items;
+    private int rear;
+    private int count;
+    private int front;
+
+    public ArrayQueue(int capacity){
+        items = new int[capacity];
+    }
+
+    public void enqueue(int item){
+        if (count == items.length){
+            throw new IllegalStateException();
+        }
+        items[rear] = item;
+        rear = (rear + 1) % items.length;
+        count++;
+    }
+
+    public int dequeue(){
+        var item = items[front++];
+        items[front] = 0;
+        front = (front + 1) % items.length;
+        count--;
+        return item;
+    }
+
+    // [60, 70, 30, 40, 50]
+    // 
+    // 5 -> 0 left % length
+    // 6 -> 1
+    // 7 -> 2
+    // 8 -> 3
+    // 9 -> 4
+    // 10 -> 0
+    // 11 -> 1
+
+    @Override
+    public String toString(){
+        return Arrays.toString(items);
+    }
+
+}
--- DataStructurePractice/CodeExercises/Main.java
+++ DataStructurePractice/CodeExercises/Main.java
@@ -1,9 +1,21 @@
 package CodeExercises;
 
 import java.util.Stack;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args){
         
+    }
+
+    //Reversing a Queue
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()){
+            stack.push(queue.remove());
+        }
+        while(!stack.isEmpty()){
+            queue.add(stack.pop());
+        }
     }
 }
