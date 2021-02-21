843c1550a18e69b500224a83f76a18119d89e32f
emersoncodes2020 <70417185+emersoncodes2020@users.noreply.github.com>
2021-02-21 00:12:21 -0500

    Stacks
    Added Notes for Stacks and exercises on how Stacks can be built and used to solve coding problems.

--- DataStructurePractice/CodeExercises/Main.java
+++ DataStructurePractice/CodeExercises/Main.java
@@ -1,12 +1,9 @@
 package CodeExercises;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args){
-        var list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        list.reverse();
-        System.out.println("done");
+        
     }
 }
